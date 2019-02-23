4e15be04d29d05101071bea5425c199004fdc0b8
Briana Nzivu <bjnzivu@gmail.com>
2019-02-23 11:52:55 +0300

    st modified index.html file with added questions and right indention

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,2 @@
-$(document).ready(function() {
-  $("#blanks form").submit(function() {
-         var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
-
-         blanks.forEach(function(blank) {
-           var userInput = $("input." + blank).val();
-           $("." + blank).text(userInput).val();
-         });
-
-         $("#story").sho();
-       });
-     });
+//business (or back-end) logic:
+var answer=$("input;radio[name=answer]:checked").val()
