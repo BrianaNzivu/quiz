b63fbb93582a84b356c5ebb14a6de3519a569f8d
Briana Nzivu <bjnzivu@gmail.com>
2019-02-23 12:43:11 +0300

    st added two questions to index.html and js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 //business (or back-end) logic:
-var answer=$("input;radio[name=answer]:checked").val()
+var answerOne=document.Quiz.r1.value;
+var answerTwo=document.Quiz.r2.value;
+var answerThree=document.Quiz.r3.value;
+var answerFour=document.Quiz.r4.value;
+var answerFive=document.Quiz.r5.value;
+var answerSix=document.Quiz.r6.value;
+var answerSeven=document.Quiz.r7.value;
+var answerEight=document.Quiz.r8.value;
