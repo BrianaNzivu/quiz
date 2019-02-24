b6f59cfe2af71c7160a8fd80f39e5770a6186fff
Briana Nzivu <bjnzivu@gmail.com>
2019-02-24 22:18:38 +0300

    ft javascript code that might work

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,51 @@
 $(document).ready(function(){
-  $("button#submit").click(function(){
-  $("input#submit").click(function(event){
+  object.onclick=function(){Submit}
+    $("form#Quiz").submit(function(event) {
     event.preventDefault();
-  var answer1=$("input:radio[name=r1]:checked").val();
-  var answer2=$("input:radio[name=r2]:checked").val();
-  var answer3=$("input:radio[name=r3]:checked").val();
-  var answer4=$("input:radio[name=r4]:checked").val();
-  var answer5=$("input:radio[name=r5]:checked").val();
-  var answer6=$("input:radio[name=r6]:checked").val();
-  var answer7=$("input:radio[name=r7]:checked").val();
-  var answer8=$("input:radio[name=r8]:checked").val();
-  var answer9=$("input:radio[name=r9]:checked").val();
-  var answer10=$("input:radio[name=r10]:checked").val();
+  var one=parseInt($("input:radio[name=r1]:checked").val());
+  var two=parseInt($("input:radio[name=r2]:checked").val());
+  var three=parseInt($("input:radio[name=r3]:checked").val());
+  var four=parseInt($("input:radio[name=r4]:checked").val());
+  var five=parseInt($("input:radio[name=r5]:checked").val());
+  var six=parseInt($("input:radio[name=r6]:checked").val());
+  var seven=parseInt($("input:radio[name=r7]:checked").val());
+  var eight=parseInt($("input:radio[name=r8]:checked").val());
+  var nine=parseInt($("input:radio[name=r9]:checked").val());
+  var ten=parseInt($("input:radio[name=r10]:checked").val());
+
   var marks=0
 
-  if (answer1=="B") {
+  if (one==="B") {
           marks+=10;
   }
-  if (answer2=="A") {
+  if (two==="A") {
           marks+=10;
   }
-  if (answer3=="A") {
+  if (three==="A") {
           marks+=10;
   }
-  if (answer4=="B") {
+  if (four==="B") {
           marks+=10;
   }
-  if (answer5=="B") {
+  if (five==="B") {
           marks+=10;
   }
-  if (answer6=="C") {
+  if (six==="C") {
           marks+=10;
   }
-  if (answer7=="C") {
+  if (seven==="C") {
           marks+=10;
   }
-  if (answer8=="A") {
+  if (eight==="A") {
           marks+=10;
   }
-  if (answer9=="B") {
+  if (nine==="B") {
           marks+=10;
   }
-  if (answer10=="A") {
+  if (ten==="A") {
           marks+=10;
   }
-  alert('you have'+marks)
+  var results=[one+two+three+four+five+six+seven+eight+nine+ten]
+  alert('you have'+results)
   });
-  var total=100
-  var max=100
-  var percentage=(marks/max)*total
-
-  $("#output").text(percentage);
-});
 });
