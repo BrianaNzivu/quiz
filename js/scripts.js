f4cedaa47bf1dde0a9ceb23ae2768eadccce600e
Briana Nzivu <bjnzivu@gmail.com>
2019-02-25 10:37:20 +0300

    st for real now a stable javascript code that brings the answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,20 @@
 $(document).ready(function(){
-  $("button#submit").click(function(){
-  $("input#submit").click(function(event){
-    event.preventDefault();
-  var answer1=$("input:radio[name=r1]:checked").val();
-  var answer2=$("input:radio[name=r2]:checked").val();
-  var answer3=$("input:radio[name=r3]:checked").val();
-  var answer4=$("input:radio[name=r4]:checked").val();
-  var answer5=$("input:radio[name=r5]:checked").val();
-  var answer6=$("input:radio[name=r6]:checked").val();
-  var answer7=$("input:radio[name=r7]:checked").val();
-  var answer8=$("input:radio[name=r8]:checked").val();
-  var answer9=$("input:radio[name=r9]:checked").val();
-  var answer10=$("input:radio[name=r10]:checked").val();
-  var marks=0
+  $("form#Quiz").submit(function(event) {
+  event.preventDefault();
+  var answer1=parseInt($("input:radio[name=r1]:checked").val());
+  var answer2=parseInt($("input:radio[name=r2]:checked").val());
+  var answer3=parseInt($("input:radio[name=r3]:checked").val());
+  var answer4=parseInt($("input:radio[name=r4]:checked").val());
+  var answer5=parseInt($("input:radio[name=r5]:checked").val());
+  var answer6=parseInt($("input:radio[name=r6]:checked").val());
+  var answer7=parseInt($("input:radio[name=r7]:checked").val());
+  var answer8=parseInt($("input:radio[name=r8]:checked").val());
+  var answer9=parseInt($("input:radio[name=r9]:checked").val());
+  var answer10=parseInt($("input:radio[name=r10]:checked").val());
 
-  if (answer1=="B") {
-          marks+=10;
-  }
-  if (answer2=="A") {
-          marks+=10;
-  }
-  if (answer3=="A") {
-          marks+=10;
-  }
-  if (answer4=="B") {
-          marks+=10;
-  }
-  if (answer5=="B") {
-          marks+=10;
-  }
-  if (answer6=="C") {
-          marks+=10;
-  }
-  if (answer7=="C") {
-          marks+=10;
-  }
-  if (answer8=="A") {
-          marks+=10;
-  }
-  if (answer9=="B") {
-          marks+=10;
-  }
-  if (answer10=="A") {
-          marks+=10;
-  }
-  alert('you have'+marks)
-  });
-  var total=100
-  var max=100
-  var percentage=(marks/max)*total
+var total=(answer1+answer2+answer3+answer4+answer5+answer6+answer7+answer8+answer9+answer10);
+var percent =((total/100)*100);
+  $("#results").text("You have scored:"+percent+"%");
 
-  $("#output").text(percentage);
 });
 });
